a6a4657439e6b0b11c02d3909a4baaf2fda0922c
Michael Matasyants <michael.matasyanc@gmail.com>
2023-12-12 21:56:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SECRET SANTA telegram bot
---
+++ santa_bot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SantaBotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'santa_bot'
