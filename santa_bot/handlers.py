160593b1f7ad1f3a42294e2a96777d16dd9dbb22
David Schnoll <davidschnoll@gmail.com>
2023-12-15 01:54:49 +0200

    Fetch actual game data

--- santa_bot/handlers.py
+++ santa_bot/handlers.py
@@ -3,7 +3,7 @@ from telegram.ext import CallbackContext, ConversationHandler
 from models import Game
 
 
-NAME, EMAIL, WISHLIST, CONFIRM = range(4)
+NAME, EMAIL, WISHLIST, CONFIRM, EDITING_HANDLING = range(5)
 
 
 def start(update: Update, context: CallbackContext):
@@ -11,14 +11,19 @@ def start(update: Update, context: CallbackContext):
 
 
 def start_player(update: Update, context: CallbackContext):
-    # TODO: fetch and use actual game details.
-    print(context.args)
-    message_text = "Замечательно, ты собираешься участвовать в игре: {game_details}"
+    """Reached through deep-link with game ID."""
+    game_id = context.args[0]
+    game = Game.objects.get(id=game_id)
+    message_text = f"""Замечательно, ты собираешься участвовать в игре: {game.name}
+Игра проходит с {game.start_date} по {game.end_date}.
+Краткое описание от организатора:
+{game.description}"""
     update.message.reply_text(message_text)
     update.message.reply_text(
         """Для начала скажи, как к тебе можно обращаться?
 Напиши так, чтобы другие игроки могли тебя узнать"""
     )
+    context.user_data["current_game"] = game
     return NAME
 
 
@@ -41,7 +46,11 @@ def get_email(update: Update, context: CallbackContext):
 
 def get_wishlist(update: Update, context: CallbackContext):
     context.user_data["wishlist"] = update.message.text
-    wishlist = update.message.text
+    return check_if_correct(update, context)
+
+
+def check_if_correct(update: Update, context: CallbackContext):
+    wishlist = context.user_data["wishlist"]
     name = context.user_data["name"]
     email = context.user_data["email"]
 
@@ -66,10 +75,47 @@ def confirm_participation(update: Update, context: CallbackContext):
     query = update.callback_query
     query.answer()
     if query.data == "participation_correct":
+        game = context.user_data["current_game"]
         # TODO: Create db entry.
-        message_text = """Превосходно, ты в игре! \
-{game_end_date} мы проведем жеребьевку и ты узнаешь имя и контакты своего тайного друга. \
+        message_text = f"""Превосходно, ты в игре! \
+{game.end_date} мы проведем жеребьевку и ты узнаешь имя и контакты своего тайного друга. \
 Ему и нужно будет подарить подарок!"""
         query.message.reply_text(message_text)
-
+    elif query.data == "edit_participation":
+        message_text = "Окей, что надо изменить?"
+        keyboard = [
+            [
+                InlineKeyboardButton("Имя", callback_data="edit_name"),
+                InlineKeyboardButton("Email", callback_data="edit_email"),
+                InlineKeyboardButton("Пожелания", callback_data="edit_wishlist")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        query.message.reply_text(message_text, reply_markup=reply_markup)
+        return EDITING_HANDLING
     return ConversationHandler.END
+
+
+def handle_participation_editing(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    if query.data == "edit_name":
+        message_text = "Напиши, как к тебе обращаться (чтобы другие игроки тебя могли узнать)"
+        context.user_data["now_editing"] = "name"
+    elif query.data == "edit_email":
+        message_text = "Напиши, свой email"
+        context.user_data["now_editing"] = "email"
+    elif query.data == "edit_wishlist":
+        message_text = "Напиши, что ты хочешь получить в подарок"
+        context.user_data["now_editing"] = "wishlist"
+
+    query.message.reply_text(message_text)
+    return get_name(update, context)
+
+
+def get_edited_response(update: Update, context: CallbackContext):
+    key_to_edit = context.user_data.pop("now_editing")
+    context.user_data[key_to_edit] = update.message.text
+
+    return check_if_correct(update, context)
